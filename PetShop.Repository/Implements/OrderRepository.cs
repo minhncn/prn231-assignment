7d2ae0bd9e9cff216d0064122828fbeb455822be
minhncn2310 <minh27122002pro@gmail.com>
2024-03-05 01:24:45 +0700

    Add Config for JWT

---
+++ PetShop.Repository/Implements/OrderRepository.cs
@@ -0,0 +1,69 @@
+﻿using PetShop.Business.Models;
+using PetShop.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Repositories.Implements
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly PetShopContext _context;
+
+        public OrderRepository(PetShopContext context)
+        {
+            _context = context;
+        }
+
+        public Order Create(Order entity)
+        {
+            var order = _context.Orders.Add(entity);
+            _context.SaveChanges();
+            return order.Entity;
+        }
+
+        public async Task<Order> CreateAsync(Order entity)
+        {
+            var order = await _context.Orders.AddAsync(entity);
+            _context.SaveChanges();
+            return order.Entity;
+        }
+
+        public Order Delete(Order entity)
+        {
+            var order = _context.Orders.Remove(entity);
+            _context.SaveChanges();
+            return order.Entity;
+        }
+
+        public IQueryable<Order> Get()
+        {
+            return _context.Orders;
+        }
+
+        public IQueryable<Order> Get(Expression<Func<Order, bool>> predicate)
+        {
+            return _context.Orders.Where(predicate);
+        }
+
+        public Order Get<TKey>(TKey id)
+        {
+            return _context.Orders.Find(id);
+        }
+
+        public async Task<Order> GetAsync<TKey>(TKey id)
+        {
+            return await _context.Orders.FindAsync(id);
+        }
+
+        public Order Update(Order entity)
+        {
+            var order = _context.Orders.Update(entity);
+            _context.SaveChanges();
+            return order.Entity;
+        }
+    }
+}
---
+++ PetShop.Service/Implements/JwtService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using PetShop.Business.Models;
+using PetShop.Services.Intefaces;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Services.Implements
+{
+    public class JwtService : IJwtService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["JwtSettings:SecretKey"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
